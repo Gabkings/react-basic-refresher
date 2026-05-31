a87972836489b2556acf840bbc8f0daa5bd9f05c
gabkings <gabworks51@gmail.com>
2026-05-31 04:13:39 +0300

    Adding home page

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+import Home from './pages/Home'
+import Auth from './pages/Auth'
+import Checkout from './pages/Checkout'
+
+import NaNvbar from './components/Navbar'
+
+
+function App() {
+
+  return (
+    <div className="app">
+      <NaNvbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
+     
+    </div>
+  )
+}
+
+export default App
